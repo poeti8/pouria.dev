7d0e96d048ffb7b12e41ea4785e5932ebc3d2385
Pouria Ezzati <ezzati.upt@gmail.com>
2022-10-02 17:36:23 +0330

    remove cursor because of error :(

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -1,24 +1,16 @@
 import React, { FC, createContext, useState, useEffect, useRef } from "react";
-import * as cursorEffects from "cursor-effects";
 
 export const AnimationContext = createContext(true);
 
 const Root: FC<{ path: string }> = ({ children, path }) => {
   const [originalPath, setPath] = useState(path);
   const [rendered, setRendered] = useState(false);
-  const isCursorInitialized = useRef(false);
 
   useEffect(() => {
     if (originalPath !== path) {
       setRendered(true);
     }
   });
-
-  useEffect(() => {
-    if (isCursorInitialized.current) return;
-    isCursorInitialized.current = true;
-    cursorEffects.bubbleCursor();
-  }, []);
 
   return (
     <AnimationContext.Provider
